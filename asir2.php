ce68e653e881aa55f9d496b682b3adb6772cf2fa
Luis <lcamus4@gmail.com>
2019-10-11 19:54:30 +0200

    Fecha

--- asir2.php
+++ asir2.php
@@ -3,6 +3,9 @@ ASIR2
 
 <?php
 $f=fopen('visitas.txt','a');
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fwrite($f,date('Y-m-d H:i:s '));
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,"\r\n");
 fclose($f)
 ?>
+
