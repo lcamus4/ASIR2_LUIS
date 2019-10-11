2deafb64ccc4e6eb1b605b3035cb2eee9b4427df
Luis <lcamus4@gmail.com>
2019-10-11 20:23:35 +0200

    Variables

---
+++ 2a.php
@@ -0,0 +1,8 @@
+<?php
+$a= 15;
+$b= 25;
+echo "Suma:".$a."+".$b."=".($a+$b).'</br>';
+echo "Resta:".$a."-".$b."=".($a-$b).'</br>';
+echo "Multiplicar:".$a."*".$b."=".($a*$b).'</br>';
+echo "Division:".$b."/".$a."=".($b/$a);
+?>
