84cffef596e7594b0d588540d34a30aee966aa57
Luis <lcamus4@gmail.com>
2019-11-08 19:54:28 +0100

    HTML

---
+++ html.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>SRI</title>
+</head>
+<body>
+
+<h1>Decroly</h1>
+<h2 bgcolor="#E6E6FA">ASIR 2</h2>
+<h3>SRI</h3>
+<p><font color="red">Esta</font> <br> <center>es</center> <font size="6">la</font> <u>asignatura</u> <b>SRI</b></p>
+<img src="Cantabria.jpg" alt="Cantabria" style="width:500px;height:600px;">
+ <form action="/action_page.php">
+  Nombre:<br>
+  <input type="text" name="Nombre" value="Mickey"><br>
+  Apellido:<br>
+  <input type="text" name="Apellido" value="Mouse"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+ <table border: 1px solid black; style="width:100%">
+  <tr>
+    <th>Nombre</th>
+    <th>Apellido</th>
+  </tr>
+  <tr>
+    <td>Javier</td>
+    <td>Ruiz</td>
+  </tr>
+  <tr>
+    <td>Victor</td>
+    <td>Salazar</td>
+  </tr>
+</table> 
+
+</body>
+</html>
